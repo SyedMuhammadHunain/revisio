a2b426a462216ac87c2ab6866922f464b8cdfd35
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-08-20 18:42:50 +0500

    Added Side Header 3d view

--- frontend/src/app/side-header/side-header.component.ts
+++ frontend/src/app/side-header/side-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-header',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './side-header.component.html',
   styleUrl: './side-header.component.css'
 })
-export class SideHeaderComponent {
+export class SideHeaderComponent implements AfterViewInit {
+  constructor(private el: ElementRef) {}
 
+  ngAfterViewInit() {
+    const container = this.el.nativeElement.querySelector('.image-container');
+    const img = this.el.nativeElement.querySelector('img');
+
+    container.addEventListener('mousemove', (e: MouseEvent) => {
+      const { left, top, width, height } = container.getBoundingClientRect();
+      const x = e.clientX - left;
+      const y = e.clientY - top;
+
+      const rotateX = ((y / height) - 0.5) * 20; // tilt intensity
+      const rotateY = ((x / width) - 0.5) * -20;
+
+      img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+
+    container.addEventListener('mouseleave', () => {
+      img.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    });
+  }
 }
