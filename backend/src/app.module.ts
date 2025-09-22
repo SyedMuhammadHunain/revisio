6403925ee9f15445fb2d78fbc37718102308a40c
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-09-22 22:47:47 +0500

    Added mongodb atlas url

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { TestConfigModule } from './test-config/test-config.module';
 import { TestResultsModule } from './test-results/test-results.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config';
-
+import { ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -15,7 +15,12 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
 
-    MongooseModule.forRoot(process.env.MONGODB_URL!),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+    }),
     EmailModule,
     AuthModule,
     QuestionsModule,
