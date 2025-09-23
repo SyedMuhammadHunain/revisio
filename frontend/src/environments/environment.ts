f55541755913daa7e6c114a5c28c6ec968540407
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-09-23 08:00:40 +0500

    Deploying Frontend

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+// src/environments/environment.ts (PRODUCTION)
+export const environment = {
+  production: true, 
+  API_URL: 'https://revisio-production.up.railway.app', 
+  MY_CHATBOT_API_KEY: 'CPAI_0TqJUdPMJ_1SoYljJ98agbhEFEDXbcuY',
+  MODEL_API_KEY: 'AIzaSyCfpF3KzxQsxJiHvJtYYWfaDbtmKRfHSbs',
+  CHATBOT_URL: 'https://corpusai-api.vercel.app/api/chatbot/chat',
+};
