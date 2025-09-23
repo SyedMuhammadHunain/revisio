ac523002e5958402206a2d234bd08488cf4d1fea
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-09-23 09:01:26 +0500

    Internal Server error resolving

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,8 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    //origin: ['http://localhost:4200', 'https://revisio-z8e9.vercel.app'],
-    origin: true,
+    origin: ['http://localhost:4200', 'https://revisio-z8e9.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
